4859009cdcb56a829fefae9ef3b0d504ca0d2546
Al amin <alaminmridha2004@gmail.com>
2024-05-31 10:46:12 +0600

    Assignment Upload 0.1

---
+++ main.js
@@ -0,0 +1,156 @@
+// ========== Question 1 ========== //
+
+let carName = "Voivo";
+
+
+
+
+
+
+
+// ========== Question 2 ========== //
+
+// firstName = "John";
+// lastName = "Doe";
+// age = 35;
+
+
+let firstName = "John";  // write variable name
+let lastName = "Doe" //write variable values 
+let age = 35; // write variable name and values both 
+
+
+
+
+
+
+
+
+
+
+
+// ========== Question 3 ========== //
+
+let x = 10;
+let y = 5;
+x *= y; 
+
+
+
+
+
+
+
+
+
+
+// ========== Question 4 ========== //
+
+// let length = 16; // data type is number.
+// let lastName = "Johnson"; // data type is string. 
+
+// const x = {
+//   firstName: "John",  
+//   lastName: "Doe"
+// };    // data type is object. 
+
+
+
+
+
+
+
+
+// ========== Question 5 ========== //
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+
+myFunction() // call the function and see the output 
+
+
+
+
+
+
+
+
+// ========== Question 6 ========== //
+
+let person = {name:"John", age:50};
+alert(person.name + " is " + person.age);
+
+
+
+
+
+
+
+
+// ========== Question 7 ========== //
+
+// Both inline js and external js are done 
+
+// document.querySelector("button").addEventListener("click", () =>{
+//     alert("Love You Hablu Programmer");
+// });
+
+
+
+
+
+
+
+
+// ========== Question 8 ========== //
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert("array has item " + cars.length);
+
+
+const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+Brand[0] = "Ford";
+
+
+
+
+
+
+
+
+
+// ========== Question 9 ========== //
+
+// 1. Use the correct Math method to create a random number.
+console.log(Math.random());
+
+// 2. Use the correct Math method to return the largest number of 10 and 20.
+console.log(Math.max(10,20));
+
+// 3. Use the correct Math method to get the square root of 9.
+console.log(Math.sqrt(9));
+
+
+
+
+
+
+
+
+
+
+// ========== Question 10 ========== //
+
+// let x = 10;
+// let y = 5;
+
+alert(x > y) ;
+
+let myAge = 20;
+(myAge > 18)? alert("Old enough") : alert("Too young");
+
+
+
+
+// ========== Allhamdulillah ========== //
